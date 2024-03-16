ac0428219a8c83cfdde3d2b158d8f1a1b2387c7c
Saeyeon Hong <sean@Saeyeons-MacBook-Air.local>
2024-03-17 04:49:16 +0900

    flask directory changed

--- code/EwhaToolBox/public/db_handler.py
+++ code/EwhaToolBox/public/db_handler.py
@@ -3,7 +3,7 @@ import json
 
 class DBModule:
     def __init__(self):
-        with open("./EwhaToolBox/auth/firebaseAuth.json") as f:
+        with open("./auth/firebaseAuth.json") as f:
 
             config = json.load(f)
 
@@ -38,41 +38,22 @@ class DBModule:
 
     ## index.html 
     def get_ongoing(self):
-        ref = self.db.reference('posts')
+        ref = self.db.reference('post')
     
     # 레퍼런스에서 모든 게시글을 가져와서 반환
         posts = ref.get()
         return posts
     
     def get_open_design(self):
-        ref = self.db.reference('designs')
+        ref = self.db.reference('design')
 
     
     ##제품 상세
-    def post_detail(self, post_id):
-        ref = self.db.reference('posts')
+    def post_detail(self,post_id):
+        ref = self.db.reference('post')
         post = ref.child(post_id).get()
         return post
     
-    ##제품 수정
-    def update_post(self, post_id, data):
-        post = {
-            "category" : data['category'],
-            "title" : data['title'],
-            "desc" : data['desc'],
-            "period_start" : data['period_start'],
-            "period_end" : data['period_end'],
-            "price" : data['price'],
-            "min_amount" : data['min_amount'],
-            "max_amount" : data['max_amount'],
-            "image_url" : data['image_url'],
-            "option_cnt" : data['option_cnt'],
-            "status" : data['status']
-        }
-        self.db.child("posts").child(post_id).update(post)
-        return True
-
-
 
     ## 공지
     def insert_notice(self, post_id, data):
@@ -117,43 +98,59 @@ class DBModule:
             "writer_id" : data['writer_id'],
             "question_desc" : data['question_desc']
         }
-        self.db.child("question").child(post_id).ㅔpush(question)
+        self.db.child("question").child(post_id).push(question)
         return True
     
-    def question_duplicate_check(self, _id):
-        questions = self.db.child("question").get()
-        for question in questions.each():
-            if (question.key() == question_id):
-                return False
-        return True
+    # def question_duplicate_check(self, _id):
+    #     questions = self.db.child("question").get()
+    #     for question in questions.each():
+    #         if (question.key() == question_id):
+    #             return False
+    #     return True
     
 
     ## Q&A - A
-    def insert_answer(self, answer_id, data):
+    def insert_answer(self, question_id, data):
         answer = {
-            "answer_id" : data['answer_id'],
             "question_id" : data['question_id'],
             "post_id" : data['post_id'],
             "writer_id" : data['writer_id'],
             "answer_desc" : data['answer_desc']
         }
-        self.db.child("answer").child(answer_id).set(answer)
+        self.db.child("answer").child(question_id).push(answer)
         return True
     
-    def answer_duplicate_check(self, answer_id):
-        answers = self.db.child("answer").get()
-        for answer in answers.each():
-            if (answer.key() == answer_id):
-                return False
+        # def answer_duplicate_check(self, answer_id):
+        #     answers = self.db.child("answer").get()
+        #     for answer in answers.each():
+        #         if (answer.key() == answer_id):
+        #             return False
+        #     return True
+
+## 주문
+    def insert_order(self, data):
+        order = {
+            "post_id" : data['post_id'],
+            "user_id" : data['user_id'],
+            "size" : data['size'],
+            "amount" : data['amount'],
+            "deliver" : data['deliver'],
+            "address" : data['address']
+        }
+        self.db.child("order").push(order)
         return True
+    
+    # def order_duplicate_check(self, order_id):
+    #     orders = self.db.child("order").get()
+    #     for order in orders.each():
+    #         if (order.key() == order_id):
+    #             return False
+    #     return True
 
 
+##상품 등록
 
-
-
-
-
-    def insert_post(self, post_id, data):
+    def insert_post(self, data):
         post = {
             "post_id" : data['post_id'],
             "writer_id" : data['writer_id'],
@@ -169,18 +166,17 @@ class DBModule:
             "option_cnt" : data['option_cnt'],
             "status" : data['status']
         }
-        self.db.child("post").child(post_id).set(post)
+        self.db.child("post").push(post)
         return True
     
-    def user_duplicate_check(self, user_id):
-        users = self.db.child("user").get()
-        for user in users.each():
-            if (user.key() == user_id):
-                return False
-        return True
+    # def user_duplicate_check(self, user_id):
+    #     users = self.db.child("user").get()
+    #     for user in users.each():
+    #         if (user.key() == user_id):
+    #             return False
+    #     return True
     
-    def write_post(self):
-        pass
+
 
    
 
@@ -227,27 +223,7 @@ class DBModule:
                 return False
         return True
 
-    ## 주문
-    def insert_order(self, order_id, data):
-        order = {
-            "order_id" : data['order_id'],
-            "post_id" : data['post_id'],
-            "user_id" : data['user_id'],
-            "size" : data['size'],
-            "amount" : data['amount'],
-            "deliver" : data['deliver'],
-            "address" : data['address']
-        }
-        self.db.child("order").child(order_id).set(order)
-        return True
     
-    def order_duplicate_check(self, order_id):
-        orders = self.db.child("order").get()
-        for order in orders.each():
-            if (order.key() == order_id):
-                return False
-        return True
-
 
     #데이터 쓰기 체크 
     # def write_data(self, data):
