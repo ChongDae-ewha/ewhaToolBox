71e4fe5a83ec22e3ccdf8e823b92f18566ab32d9
Joo Hyun Lee <michelle8085@gmail.com>
2024-03-16 18:37:58 +0900

    충돌 해결

--- code/EwhaToolBox/views/main_views.py
+++ code/EwhaToolBox/views/main_views.py
@@ -2,22 +2,10 @@ import io
 import sys
 import json
 
-<<<<<<< HEAD:code/EwhaToolBox/app.py
-from flask import Flask, jsonify, render_template, request
-from database import DBHandler
-
-app = Flask(__name__)
-DB = DBHandler()
-
-@app.route("/")
-def view_index():
-    return render_template("index.html")
-=======
 from flask import Flask, jsonify, request, Blueprint,redirect, url_for
 
 bp=Blueprint('main',__name__,url_prefix='/')
 
->>>>>>> 9395b5b9a4c434244d9b4b0694dcfb5713895515:code/EwhaToolBox/views/main_views.py
 
 @bp.route("/")
 def index():
@@ -27,12 +15,11 @@ def index():
 def user():
     return ""
 
-<<<<<<< HEAD:code/EwhaToolBox/app.py
-@app.route("/user/signup")
+@bp.route("/user/signup")
 def view_signup():
     return render_template("signup.html")
 
-@app.route("/user/signup/submit")
+@bp.route("/user/signup/submit")
 def reg_user_submit():
     user_id = request.args.get("user_id")
     pw = request.args.get("pw")
@@ -41,28 +28,12 @@ def reg_user_submit():
     phone = request.args.get("phone")
     address = request.args.get("address")
 
-@app.route("/user/signup/post", methods=["POST"])
+@bp.route("/user/signup/post", methods=["POST"])
 def reg_user_post():
     data = request.form
     return render_template("signup_result.html", data=data)
 
 
-@app.route("/user/signin")
+@bp.route("/user/signin")
 def view_signin():
     return render_template("signin.html")
-
-def login():
-    return ""
-=======
-@bp.route("/user/signup", methods=['POST'])
-def reg_user():
-    user_id = request.args.get('user_id')
-    pw = request.args.get('pw')
-    name = request.args.get('name')
-    email = request.args.get('email')
-    phone = request.args.get('phone')
-    address = request.args.get('address')
-
->>>>>>> 9395b5b9a4c434244d9b4b0694dcfb5713895515:code/EwhaToolBox/views/main_views.py
-
-
